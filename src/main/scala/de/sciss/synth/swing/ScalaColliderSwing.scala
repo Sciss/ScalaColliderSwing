2b6286436fbd6f168adb305c13241986d12036fb
Sciss <contact@sciss.de>
2011-11-29 23:48:14 +0000

    start 0.32-SNAPSHOT

--- src/main/scala/de/sciss/synth/swing/ScalaColliderSwing.scala
+++ src/main/scala/de/sciss/synth/swing/ScalaColliderSwing.scala
@@ -33,9 +33,9 @@ import scala.swing.Swing
 
 object ScalaColliderSwing {
    val name          = "ScalaCollider-Swing"
-   val version       = 0.31
+   val version       = 0.32
    val copyright     = "(C)opyright 2008-2011 Hanns Holger Rutz"
-   val isSnapshot    = false
+   val isSnapshot    = true
 
    def versionString = {
       val s = (version + 0.001).toString.substring( 0, 4 )
